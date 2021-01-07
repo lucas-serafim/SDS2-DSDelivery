9cf335ad69899b6558d0755abf8fd5067551b5e8
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-07 15:17:44 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Hello from './Hello'
+
+function App() {
+   return (
+      <div className="App">
+         <h1>Hello!</h1>
+      </div>
+   );
+}
+
+export default App;
