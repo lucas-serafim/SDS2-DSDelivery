9cab20d3dfd74a0d21a0488cc7a48fe949c8ad0d
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-08 12:00:16 -0300

    Add routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -14,7 +14,7 @@ function Footer() {
             <a href = "https://www.linkedin.com/school/devsuperior" target = "_new">
                <LinkedinIcon />
             </a>
-            <a href = "https://www.instagram.com/devsuperior.ig/">
+            <a href = "https://www.instagram.com/devsuperior.ig/" target = "_new">
                <InstagramIcon />
             </a>
          </div>
