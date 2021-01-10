4adb9580f6d1110af58c41a03a9aa87d187b53d9
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-10 14:30:19 -0300

    Finish front-web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import AsyncSelect from 'react-select/async'
-import { NumberLiteralType } from 'typescript';
 import { fetchLocalMapBox } from '../api';
-import { OrderLocationdata } from './types';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
    lat: 51.505,
@@ -20,7 +19,7 @@ type Place = {
 }
 
 type Props = {
-   onChangeLocation: (location: OrderLocationdata) => void
+   onChangeLocation: (location: OrderLocationData) => void
 }
 
 function OrderLocation({ onChangeLocation } : Props) {
