ef34bb50902bf29f9f80c39485fffc0def98e20e
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-12 12:44:50 -0300

    Add orders list

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,19 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { Image, StyleSheet, Text, View } from "react-native"
 import { RectButton } from 'react-native-gesture-handler'
+import Header from '../Header'
 
 function Home() {
+   const navigation = useNavigation()
 
    const handleOnPress = () => {
-      
+      navigation.navigate('Orders')
    }
 
    return (
       <>
+         <Header />
          <View style = {styles.container}>
             <Image source = {require('../assets/deliveryman.png')} />
             <Text style = {styles.title}>Acompanhe os pedidos e {'\n'} entregue no prazo!</Text>
@@ -17,7 +21,7 @@ function Home() {
          </View>
 
          <View style = {styles.footer}>
-            <RectButton style = {styles.button} onPress = {}>
+            <RectButton style = {styles.button} onPress = {handleOnPress}>
                <Text style = {styles.buttonText}>VER PEDIDOS</Text>
             </RectButton>
          </View>
